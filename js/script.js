ec6949020e1a779fa0d89bbff87a0b01621c5957
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-09 20:04:36 +0300

    Add new features
    1. Add calculator heading and description
    2. Create main calculator layout
    3. Implement screen and result components
    4. Add buttons grid and button elements
    5. Bind button click event to handleButtonClick function
    
    [Ticket: 2]

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-//Script for the calculator
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "×":
+            return multiply(firstNumber, secondNumber);
+        case "÷":
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+function handleButtonClick(number) {
+    let displayElement = document.getElementById("currentResult");
+    let currentNumber = displayElement.innerHTML;
+    let displayElement2 = document.getElementById("nextResult");
+    let nextNumber = displayElement2.innerHTML;
+    
+
+        if (displayElement.innerHTML === "0") {
+            displayElement.innerHTML = "";
+            displayElement.innerHTML = number;
+
+        }else if (displayElement.innerHTML !== "0") {
+            displayElement.innerHTML += number;
+        }
+  }
+
